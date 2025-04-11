f9cd2e4739749848b3838f19fe755953135e7ce2
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-04-11 23:29:49 +0900

    Improve preprocessing (#2)
    * update to improve preprocessing
    
    * Revert "update to improve preprocessing"
    
    This reverts commit a5555ed1822698aa0783fcca246902459d88878a.
    
    * update for `DiscretizeBboxes`
    
    * update

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,10 @@ from layout_prompter.datasets import load_poster_layout
 
 
 @pytest.fixture
-def raw_dataset() -> ds.DatasetDict:
+def raw_hf_dataset() -> ds.DatasetDict:
     return load_poster_layout(return_raw=True)
 
 
 @pytest.fixture
-def dataset() -> ds.DatasetDict:
+def hf_dataset() -> ds.DatasetDict:
     return load_poster_layout()
--- tests/preprocessors/content_aware_processor_test.py
+++ tests/preprocessors/content_aware_processor_test.py
@@ -6,20 +6,20 @@ from layout_prompter.utils.testing import LayoutPrompterTestCase
 
 
 class TestContentAwareProcessor(LayoutPrompterTestCase):
-    def test_content_aware_processor(self, raw_dataset: ds.DatasetDict):
+    def test_content_aware_processor(self, dataset: ds.DatasetDict):
         settings = PosterLayoutSettings()
 
         processor = ContentAwareProcessor(
             canvas_size=settings.canvas_size,
         )
-        raw_dataset = processor.invoke(
-            raw_dataset,
+        processed_dataset = processor.invoke(
+            dataset,
             config={"configurable": {"num_proc": 32}},
         )
-        assert isinstance(raw_dataset, ds.DatasetDict)
+        assert isinstance(dataset, ds.DatasetDict)
 
         dataset_dir = self.FIXTURES_ROOT / "datasets" / "poster-layout" / "processed"
 
         if not dataset_dir.exists():
             dataset_dir.mkdir(parents=True, exist_ok=True)
-            raw_dataset.save_to_disk(dataset_dir)
+            processed_dataset.save_to_disk(dataset_dir)
