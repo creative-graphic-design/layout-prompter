5223ad91bfc1fccca08de2e696fc3a80e87ea45c
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-06-24 16:54:32 +0900

    refactor(layout-prompter): add type hint for bboxes parameter in normalize_bboxes function

--- src/layout_prompter/utils/bbox.py
+++ src/layout_prompter/utils/bbox.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def normalize_bboxes(bboxes, w: int, h: int) -> np.ndarray:
+def normalize_bboxes(bboxes: np.ndarray, w: int, h: int) -> np.ndarray:
     """Normalize bounding boxes to [0, 1] range."""
     assert bboxes.shape[1] == 4, "bboxes should be of shape (N, 4)"
 
