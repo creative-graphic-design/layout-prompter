3f6ad4557b0278933440193e8bda728be5db463d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-07-11 00:07:09 +0900

    refactor: remove unused bbox functions and clean up imports

--- src/layout_prompter/utils/bbox.py
+++ src/layout_prompter/utils/bbox.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import numpy as np
 
 
@@ -9,23 +11,3 @@ def normalize_bboxes(bboxes: np.ndarray, w: int, h: int) -> np.ndarray:
     bboxes[:, 0::2] /= w
     bboxes[:, 1::2] /= h
     return bboxes
-
-
-def decapsulate(bboxes: np.ndarray):
-    if len(bboxes.shape) == 2:
-        return bboxes.T
-    else:
-        return np.transpose(bboxes, (2, 0, 1))
-
-
-def convert_ltwh_to_ltrb(bbox: np.ndarray):
-    if len(bbox.shape) == 1:
-        left, top, width, height = bbox
-        right = left + width
-        bottom = top + height
-        return np.array((left, top, right, bottom))
-
-    left, top, width, height = decapsulate(bbox)
-    right = left + width
-    bottom = top + height
-    return np.stack([left, top, right, bottom], axis=-1)
