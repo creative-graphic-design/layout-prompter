31caa6d6a074497f9b81a64c61d3378c82c7017e
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-07-16 00:56:39 +0900

    feat: add base and content-aware serializers for layout generation

---
+++ src/layout_prompter/modules/serializers/__init__.py
@@ -0,0 +1,9 @@
+from .base import LayoutSerializer, LayoutSerializerConfig, LayoutSerializerInput
+from .content_aware_serializer import ContentAwareSerializer
+
+__all__ = [
+    "LayoutSerializer",
+    "LayoutSerializerConfig",
+    "LayoutSerializerInput",
+    "ContentAwareSerializer",
+]
