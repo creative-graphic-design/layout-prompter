a2278785c1cbb8b6319bedeb76a2c8ebc1eecd55
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-07-16 01:48:53 +0900

    refactor: comment out unused imports and methods in GenTypeSizeProcessor

--- src/layout_prompter/preprocessors/gen_type_size_processor.py
+++ src/layout_prompter/preprocessors/gen_type_size_processor.py
@@ -1,14 +1,14 @@
-from typing import Optional
+# from typing import Optional
 
-from langchain_core.runnables.config import RunnableConfig
+# from langchain_core.runnables.config import RunnableConfig
 
-from layout_prompter.models import ProcessedLayoutData
-from layout_prompter.models.layout_data import LayoutData
-from layout_prompter.transforms import (
-    DiscretizeBboxes,
-    LabelDictSort,
-    ShuffleElements,
-)
+# from layout_prompter.models import ProcessedLayoutData
+# from layout_prompter.models.layout_data import LayoutData
+# from layout_prompter.transforms import (
+#     DiscretizeBboxes,
+#     LabelDictSort,
+#     ShuffleElements,
+# )
 
 from .base import Processor
 
@@ -16,13 +16,9 @@ from .base import Processor
 class GenTypeSizeProcessor(Processor):
     name: str = "gen-type-size-processor"
 
-    def _invoke(
-        self, layout_data: LayoutData, config: Optional[RunnableConfig] = None, **kwargs
-    ) -> ProcessedLayoutData:
-        chain = (
-            ShuffleElements()
-            | LabelDictSort()
-            | DiscretizeBboxes(target_canvas_size=self.target_canvas_size)
-        )
-        chain
-        breakpoint()
+    # def _invoke(
+    #     self, layout_data: LayoutData, config: Optional[RunnableConfig] = None, **kwargs
+    # ) -> ProcessedLayoutData:
+    #     chain = ShuffleElements() | LabelDictSort()
+
+    #     breakpoint()
